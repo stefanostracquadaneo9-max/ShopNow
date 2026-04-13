325baac858d0f66adc717e54592f258cecdf4600
stefanostracquadaneo9-max <stefanostracquadaneo9@gmail.com>
2026-04-13 02:22:49 +0200

    Delete Desktop/sito directory

--- Desktop/sito/open-offline.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title ShopNow Offline
-echo Modalita' offline avviata.
-echo Attenzione: checkout, Stripe ed email NON funzionano offline.
-start "" "file:///C:/Users/stefa/Desktop/sito/index.html"
