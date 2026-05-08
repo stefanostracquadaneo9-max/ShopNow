d44fcee4b968cc2cd9737163d67fef476aab3ff2
Stefano Stracquadaneo <stefanostracquadaneo9@gmail.com>
2026-05-09 00:21:13 +0200

    Remove unused check scripts

--- check_restore.js
+++
@@ -1,6 +0,0 @@
-const Database = require('better-sqlite3');
-const db = Database('app.db');
-
-const product = db.prepare('SELECT stock FROM products WHERE id = 999').get();
-console.log('Stock dopo ripristino:', product.stock);
-db.close();
